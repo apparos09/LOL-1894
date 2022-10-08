8bdd7615524d053a40729ec2e915bbc1e09bdd47
mecha-rm <roderick.montague243@gmail.com>
2022-10-08 00:17:32 -0400

    Set game to 30 FPS.
    Set the game's framerate.

---
+++ Assets/RoderickMontague/Scripts/TitleManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    // Awake is called when the script instance is loaded.
+    private void Awake()
+    {
+        // set to 30 FPS
+        Application.targetFrameRate = 30;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
