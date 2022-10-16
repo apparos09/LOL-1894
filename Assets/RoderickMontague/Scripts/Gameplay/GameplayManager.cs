5f0cde436cd66ffb4dc1360a2014b3315611d0af
mecha-rm <roderick.montague243@gmail.com>
2022-10-15 23:25:22 -0400

    Touch Check Seems to Work Fine
    The touch system seems to pick up new touches pretty well. I don't think you need to store past touches, so this should be fine for getting input from the user's touch.

--- Assets/RoderickMontague/Scripts/Gameplay/GameplayManager.cs
+++ Assets/RoderickMontague/Scripts/Gameplay/GameplayManager.cs
@@ -18,10 +18,19 @@ public class GameplayManager : MonoBehaviour
     // the manager for the battle.
     public BattleManager battle;
 
+    // the input from the mouse and touch screen.
+    public MouseTouchInput mouseTouchInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Tries to find the object.
+        if (mouseTouchInput == null)
+            mouseTouchInput = FindObjectOfType<MouseTouchInput>();
+
+        // Adds a component.
+        if (mouseTouchInput == null)
+            mouseTouchInput = GetComponent<MouseTouchInput>();
     }
 
     // // Called when a level has been loaded.
@@ -32,18 +41,22 @@ public class GameplayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount != 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        // if(Input.touchCount != 0)
+        // {
+        //     Touch touch = Input.GetTouch(0);
+        // 
+        //     Debug.Log("Finger has touched screen. Tap Count: " + touch.tapCount);
+        // 
+        //     // // checks to see if the user has touched it.
+        //     // if (touch.phase == TouchPhase.Began)
+        //     // {
+        //     //     // Debug.Log("Finger has touched screen.");
+        //     // }
+        // }
 
-            Debug.Log("Finger has touched screen. Tap Count: " + touch.tapCount);
-
-            // // checks to see if the user has touched it.
-            // if (touch.phase == TouchPhase.Began)
-            // {
-            //     // Debug.Log("Finger has touched screen.");
-            // }
-        }
+        // Checks how many touches there are.
+        if(mouseTouchInput.currentTouches.Count > 0)
+            Debug.Log("Touch Count: " + mouseTouchInput.currentTouches.Count);
 
 
         // Checks the state variable to see what kind of scene the game is in.
