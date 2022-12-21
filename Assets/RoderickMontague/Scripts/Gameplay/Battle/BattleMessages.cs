2f7affb3637f2ef76a599a88a20b7f581bbdca2d
mecha-rm <roderick.montague243@gmail.com>
2022-12-20 22:00:47 -0500

    Took out unused includes.
    Took out some unused packages that were being used in this script.

--- Assets/RoderickMontague/Scripts/Gameplay/Battle/BattleMessages.cs
+++ Assets/RoderickMontague/Scripts/Gameplay/Battle/BattleMessages.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SimpleJSON;
-using System.Security.Cryptography;
-using static UnityEngine.GraphicsBuffer;
 
 namespace RM_BBTS
 {
