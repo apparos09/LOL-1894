91e06c33ef5d1c828212edaa0a05e07a3138265b
mecha-rm <roderick.montague243@gmail.com>
2022-12-31 17:41:39 -0500

    Working on the Move Comparison Panel
    Working on the code for a panel that compares moves to one another.

---
+++ Assets/RoderickMontague/Scripts/Gameplay/Moves/Info/MoveComparisonWindow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RM_BBTS
+{
+    // The script for the move comparisons.
+    public class MoveComparisonWindow : MonoBehaviour
+    {
+        //  Player object.
+        public Player player;
+
+        [Header("Moves")]
+        // The 4 standard moves.
+        public MoveComparePanel move0;
+        public MoveComparePanel move1;
+        public MoveComparePanel move2;
+        public MoveComparePanel move3;
+
+        // The charge and run move info.
+        public MoveComparePanel chargeMove;
+        public MoveComparePanel runMove;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
